f69f429bdf7d9d4348319b18f265abd316fa5997
romanmatv <romanmatv1@gmail.com>
2022-11-13 17:25:52 +0700

    тест перевода на таймеры

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@ bool leden = 1;
 //Порог акселерометра, при котором срабатывает мигалка
 const uint gValueBlink = 15;
 
+struct repeating_timer timer;
+
 void accelerometer_interrupt_handler(uint gpio, uint32_t events) {
     printf("interrupt! reasons: 0x%x\n",
      accelerometer.getInterruptSources()); // get interrupt reasons and clear latched motion interrupts
@@ -36,8 +38,12 @@ bool blink_timer(struct repeating_timer *t) {
 
 //Функция таймера отключения блинка
 int64_t blinkoff_callback(alarm_id_t id, void *user_data) {
-    blink = false
+    printf("BLINK OFF\n");
+    blink = false;
+    leden = true;
     // Can return a value here in us to fire in the future
+    cancel_repeating_timer(&timer);
+    gpio_put(LED_PIN, 1);
     return 0;
 }
 
@@ -47,6 +53,8 @@ int main()
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    gpio_put(LED_PIN, 1);
 
     accelerometer = ADXL345();
     accelerometer.begin(ADXL345_DEFAULT_ADDRESS, // I2C address
@@ -61,14 +69,9 @@ int main()
 
     gpio_set_irq_enabled_with_callback(interrupt_pin, GPIO_IRQ_EDGE_RISE, true, &accelerometer_interrupt_handler);
 
-    struct repeating_timer timer;
-    add_repeating_timer_ms(500, blink_timer, NULL, &timer);
 
     while (true) {
-        /*tmr++;
-        if (blink){
-            tmr2++;
-        }*/
+
         accelerometer.getEvent(&sensEvent);
 
         //sensEvent.acceleration;
@@ -78,36 +81,21 @@ int main()
 
         float max = MAX(MAX(x, y), z);
 
+        /* 
+        TODO: 
+            устранить баг множественного назначения таймеров
+            Таймер блинка тупо не назначать, если он уже есть
+            А таймер остановки блинка - обнулять
+            А вообще, все это можно заменить одним таймером, который будет сам обнуляться после n раз равное sec_stop / sec_blink
+        */
         if (max>gValueBlink){
+            printf("START BLINK\n");
             blink = true;
             // Call alarm_callback in 2 seconds
-            add_alarm_in_ms(2000, blinkoff_callback, NULL, false);
-        }
-        /*
-
-        if (tmr>maxTmr){
-            tmr = 0;
-            if (blink) leden = !leden;
-        }
-        if (tmr2>maxTmr2){
-            tmr2 = 0;
-            blink = false;
-            leden = 1;
+            add_alarm_in_ms(5000, blinkoff_callback, NULL, false);
+            add_repeating_timer_ms(300, blink_timer, NULL, &timer);
         }
 
-        gpio_put(LED_PIN, leden);*/
-
-        
-        /*printf("X: %f Y: %f Z: %f\n", 
-            // note: these are raw values, to get real gravitational force, you can use accelerometer.getEvent()
-            //accelerometer.getX(),
-            //accelerometer.getY(),
-            //accelerometer.getZ(),
-
-            sensEvent.acceleration.x,
-            sensEvent.acceleration.y,
-            sensEvent.acceleration.z
-        );*/
         printf("acc: %f\n", max);
 
         sleep_ms(100);
